442c547a5ba7555732746aefdeec4d904d4d9238
Nate Schainblatt <nschainblatt@gmail.com>
2024-07-07 04:52:12 -0400

    Add undo tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -31,5 +31,11 @@ return {
         }
       end,
     },
+    {
+      'mbbill/undotree',
+      config = function()
+        vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+      end,
+    },
   },
 }
