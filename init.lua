8bef8306d088e8656bf1bc6afa8d8a2c38e3447b
Nate Schainblatt <nschainblatt@gmail.com>
2024-07-07 03:37:30 -0400

    line number coloring

--- init.lua
+++ init.lua
@@ -105,5 +105,8 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- [[ Custom configuration, last to overwrite theme and other plugin settings (Line number coloring, etc...) ]] --
+require 'config'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
