89b31eb5c21daff475f5bdcb2752db1ae7991877
Nate Schainblatt <nschainblatt@gmail.com>
2024-07-07 03:25:12 -0400

    Add vim-fugitive

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+    end,
+  },
+}
