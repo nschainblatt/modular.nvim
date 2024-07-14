a3e7d7a8f1430857c1aeeac1945c7bd010d2c2b8
Nate Schainblatt <nschainblatt@gmail.com>
2024-07-14 13:58:36 -0400

    Add and set tokyodark theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -85,4 +85,14 @@ return {
   --   'Exafunction/codeium.vim',
   --   event = 'BufEnter',
   -- },
+  {
+    "tiagovla/tokyodark.nvim",
+    opts = {
+        -- custom options here
+    },
+    config = function(_, opts)
+        require("tokyodark").setup(opts) -- calling setup is optional
+        vim.cmd [[colorscheme tokyodark]]
+    end,
+}
 }
