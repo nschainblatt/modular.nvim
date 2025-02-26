0e64e861d857e44b4d9050a316d5a66f107d726f
nschainblatt <nate@bluearrowmail.com>
2025-02-25 21:24:03 -0500

    make shell os specific, make terminal appear on right

--- lua/custom/plugins/toggle-term.lua
+++ lua/custom/plugins/toggle-term.lua
@@ -1,10 +1,18 @@
+local shell = vim.o.shell
+local os_name = vim.loop.os_uname().sysname
+if os_name == 'Windows_NT' then
+  shell = 'pwsh.exe'
+end
+
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
   config = function()
     require('toggleterm').setup {
       open_mapping = [[<c-\>]],
-      direction = 'float',
+      size = 100,
+      direction = 'vertical',
+      shell = shell,
     }
   end,
 }
